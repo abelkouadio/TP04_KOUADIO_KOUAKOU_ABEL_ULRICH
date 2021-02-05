d0ed8a07d22dbe50b55192eb41d8c78f4364be7c
abelkouadio <kouadioabel803@gmail.com>
2021-02-05 20:27:48 +0000

    second commit

---
+++ src/java/CompteBancaire/CompteBancaire.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CompteBancaire;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author MSI
+ */
+@Entity
+public class CompteBancaire implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+        private String firstName;
+        private String lastName;
+    private String accountNumber;
+  private double  balance;
+
+ public CompteBancaire() {
+    }
+
+    public CompteBancaire(String nom, String prenom, String login, double balance) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public CompteBancaire(Long id, String firstName, String lastName, String accountNumber, double balance) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof CompteBancaire)) {
+            return false;
+        }
+        CompteBancaire other = (CompteBancaire) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "CompteBancaire.CompteBancaire[ id=" + id + " ]";
+    }
+    
+}
---
+++ src/java/CompteSessionBean/CompteSessionBean.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CompteSessionBean;
+
+import CompteBancaire.CompteBancaire;
+import java.util.Collection;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author MSI
+ */
+@Stateless
+public class CompteSessionBean {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+      @PersistenceContext
+    private EntityManager em;
+
+    public void creerCompteDeTest() {
+        creeUtilisateur("John", "Lennon", "jlennon",20000.0);
+      
+    }
+     public CompteBancaire creeUtilisateur(String firstName, String lastName, String accountNumber,double balance) {
+        CompteBancaire u = new CompteBancaire(firstName, lastName, accountNumber, balance);
+        em.persist(u);
+        return u;
+    }
+     
+     public Collection<CompteBancaire> getAllUsers() {
+        // Exécution d'une requête équivalente à un select *
+        Query q = em.createQuery("select u from CompteBancaire u");
+        return q.getResultList();
+    }
+}
